fae3174b20223f2f03420b1bf660c91c8a18bfff
=kanishq1 <=kanishqkhandelwal@gmail.com>
2021-01-09 00:20:11 +0530

    initial google login setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { GoogleLogin } from "react-google-login";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [user, setUser] = useState();
+	const [err, setErr] = useState();
+	const onSuccess = (response) => {
+		setUser(response.profileObj);
+		console.log(response.profileObj);
+	};
+	const onFailure = (response) => {
+		setErr("use BITS Mail");
+		console.log(response);
+	};
+	return (
+		<div className="App">
+			<GoogleLogin
+				clientId="225636155935-141j2c5ngbfq8lk0sq6t4q7prqdedqd6.apps.googleusercontent.com"
+				buttonText="Login With BITS Mail"
+				onSuccess={onSuccess}
+				onFailure={onFailure}
+				cookiePolicy={"single_host_origin"}
+				hostedDomain={"goa.bits-pilani.ac.in"}
+			/>
+			{user && (
+				<div>
+					<p>
+						Name : {user.givenName} {user.familyName}
+					</p>
+					<img src={user.imageUrl} alt="profile" />
+				</div>
+			)}
+			{err && <p>{err}</p>}
+		</div>
+	);
 }
 
 export default App;
